5444fb172b57a18fbfe6b0039187bec5968f4acc
Ramon Brito <ramon.mecatec@gmail.com>
2023-06-17 16:34:48 -0300

    Add aplicacao 'graficos' a lista de aplicacoes do django

--- cotacao_dolar/cotacao_dolar/settings.py
+++ cotacao_dolar/cotacao_dolar/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'graficos.apps.GraficosConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
