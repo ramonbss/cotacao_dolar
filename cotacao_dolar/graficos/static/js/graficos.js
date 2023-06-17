e0dd7d9ea6bd332c73e194a34eb12b094a893dee
Ramon Brito <ramon.mecatec@gmail.com>
2023-06-17 18:47:18 -0300

    Add arquivos iniciais de template do app graficos
    - Criada pastas: static, css, js
    - Arquivos html, js, css iniciais

---
+++ cotacao_dolar/graficos/static/js/graficos.js
@@ -0,0 +1,33 @@
+function plotar_valores(moeda_alvo, valores, datas)
+{
+    const chart = Highcharts.chart("graficos-container", {
+      chart: {
+        type: "line",
+      },
+      title: {
+        text: "Cotacao Dólar x" + moeda_alvo,
+      },
+      xAxis: {
+        title:{
+            text:'Data'
+        },
+        categories: datas,
+      },
+      yAxis: {
+        title: {
+          text: "Valor em Dólar",
+        },
+      },
+      series: [
+        {
+          name: "Cotação",
+          data: valores,
+        },
+      ],
+    });
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    plotar_valores('Real', [1,2,3],['01/01','02/01','03/01'])
+    
+  });
