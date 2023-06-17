f449278df4b5e4e9366445efa277274c45439f66
Ramon Brito <ramon.mecatec@gmail.com>
2023-06-17 20:12:58 -0300

    Add selecionadores de data; Add campo de mensagem de erros

--- cotacao_dolar/graficos/static/js/graficos.js
+++ cotacao_dolar/graficos/static/js/graficos.js
@@ -27,7 +27,24 @@ function plotar_valores(moeda_alvo, valores, datas)
     });
 }
 
+function inicializar_data_pickers()
+{
+$("#data-inicio").datepicker({
+    format: "dd/mm/yyyy",
+    language: "pt-BR",
+    container: "#datepicker-dropbox-inicio",
+    });
+
+    $("#data-fim").datepicker({
+    format: "dd/mm/yyyy",
+    language: "pt-BR",
+    container: "#datepicker-dropbox-fim",
+    });
+    console.log("Data picker inicializados.")
+}
+
 document.addEventListener("DOMContentLoaded", function () {
+    inicializar_data_pickers();
     plotar_valores('Real', [1,2,3],['01/01','02/01','03/01'])
     
   });
