6754a7f6cc16290a9d64ba2af36a544fd7a55437
Ramon Brito <ramon.mecatec@gmail.com>
2023-06-19 17:59:42 -0300

    Renomear cotacao

--- cotacao_dolar/graficos/models.py
+++ cotacao_dolar/graficos/models.py
@@ -3,5 +3,5 @@ from django.db import models
 
 class CotacaoDolar(models.Model):
     moeda = models.CharField(max_length=3, null=False)
-    valor = models.FloatField(null=False)
+    cotacao = models.FloatField(null=False)
     data = models.DateField(null=False)
