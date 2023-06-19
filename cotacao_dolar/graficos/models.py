ff8f0c04b7b2f1ff4318a9f1752c3fbdd2f29be7
Ramon Brito <ramon.mecatec@gmail.com>
2023-06-19 16:16:53 -0300

    Add model CotacaoDolar
    Database de historico das cotacoes

--- cotacao_dolar/graficos/models.py
+++ cotacao_dolar/graficos/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class CotacaoDolar(models.Model):
+    moeda = models.CharField(max_length=3, null=False)
+    valor = models.FloatField(null=False)
+    data = models.DateField(null=False)
