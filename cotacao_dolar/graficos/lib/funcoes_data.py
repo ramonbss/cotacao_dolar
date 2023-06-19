2b1bfda25ae894655cf220ee3217ee0b1529fb38
Ramon Brito <ramon.mecatec@gmail.com>
2023-06-19 19:07:15 -0300

    Add metodos checar_se_dia_util e contar_dias_uteis

--- cotacao_dolar/graficos/lib/funcoes_data.py
+++ cotacao_dolar/graficos/lib/funcoes_data.py
@@ -1,5 +1,8 @@
-from datetime import datetime
+from datetime import date
+from datetime import datetime, timedelta
 import holidays
+
+feriados = holidays.Brazil()
 
 
 def converter_data(data: str) -> datetime:
@@ -14,3 +17,26 @@ def converter_data(data: str) -> datetime:
         datetime: Objeto datetime pronto para ser salvo no banco de dados
     """
     return datetime.strptime(data, "%Y-%m-%d")
+
+
+def checar_se_dia_util(data):
+    # Checa se dia da semana (0- Segunda .. 5- Sabado, 6- Domingo)
+    if data.weekday() < 5:
+        # Checa se data é um feriado
+        if data not in feriados:
+            return True
+    return False
+
+
+def contar_dias_uteis(data_inicio: datetime, data_fim: datetime) -> int:
+
+    total_dias_entre_as_datas = (data_fim - data_inicio).days + 1
+    dias_uteis = 0
+
+    for i in range(total_dias_entre_as_datas):
+        data_atual = data_inicio + timedelta(days=i)
+
+        if checar_se_dia_util(data_atual):
+            dias_uteis += 1
+
+    return dias_uteis
