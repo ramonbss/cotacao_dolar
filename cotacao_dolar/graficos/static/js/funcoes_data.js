5a77ca58a8c0d9d5eecd2ae84217ab47c12034a4
Ramon Brito <ramon.mecatec@gmail.com>
2023-06-18 23:52:07 -0300

    Add metodo criarDataObjAPartirFormatoBrasil

--- cotacao_dolar/graficos/static/js/funcoes_data.js
+++ cotacao_dolar/graficos/static/js/funcoes_data.js
@@ -28,6 +28,15 @@ export function inicializar_data_pickers() {
   console.log("Data picker inicializados.");
 }
 
+export function criarDataObjAPartirFormatoBrasil(strDataFormatoBrasil) {
+  let camposDaData = strDataFormatoBrasil.split("/");
+  return new Date(
+    parseInt(camposDaData[2]),
+    parseInt(camposDaData[1]) - 1,
+    parseInt(camposDaData[0])
+  );
+}
+
 function validarCamposDeData() {
   let inputDataInicio = document.getElementById("data-inicio");
   let inputDataFim = document.getElementById("data-fim");
@@ -72,14 +81,9 @@ function validarData(strData) {
   let regex = /^\d{2}\/\d{2}\/\d{4}$/;
   if (!regex.test(strData)) return false;
 
-  let camposDaData = strData.split("/");
-  let dia = parseInt(camposDaData[0], 10);
-  let mes = parseInt(camposDaData[1], 10) - 1;
-  let ano = parseInt(camposDaData[2], 10);
-
   // Cria um objeto Date para validar as entradas
   // Um dia 35 seria validado pelo o regex, mas daria erro aqui
-  let data = new Date(ano, mes, dia);
+  let data = criarDataObjAPartirFormatoBrasil(strData);
   return (
     data.getDate() === dia &&
     data.getMonth() === mes &&
@@ -88,19 +92,8 @@ function validarData(strData) {
 }
 
 function checarIntervaloEntreDatas(dataInicio, dataFim) {
-  let camposData1 = dataInicio.split("/");
-  let camposData2 = dataFim.split("/");
-
-  let dia1 = parseInt(camposData1[0], 10);
-  let mes1 = parseInt(camposData1[1], 10) - 1;
-  let ano1 = parseInt(camposData1[2], 10);
-
-  let dia2 = parseInt(camposData2[0], 10);
-  let mes2 = parseInt(camposData2[1], 10) - 1;
-  let ano2 = parseInt(camposData2[2], 10);
-
-  let dateObj1 = new Date(ano1, mes1, dia1);
-  let dateObj2 = new Date(ano2, mes2, dia2);
+  let dateObj1 = criarDataObjAPartirFormatoBrasil(dataInicio);
+  let dateObj2 = criarDataObjAPartirFormatoBrasil(dataFim);
 
   let diferencaEntreDatas = dateObj2.getTime() - dateObj1.getTime();
   let diferencaEmDias = Math.ceil(diferencaEntreDatas / (1000 * 3600 * 24));
