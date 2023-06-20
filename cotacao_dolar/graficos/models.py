ad45888beeec8ca723d1c3e87ebc24fbd2dd3224
Ramon Brito <ramon.mecatec@gmail.com>
2023-06-19 22:29:02 -0300

    Customizar __str__ do modelo CotacaoDolar

--- cotacao_dolar/graficos/models.py
+++ cotacao_dolar/graficos/models.py
@@ -5,3 +5,6 @@ class CotacaoDolar(models.Model):
     moeda = models.CharField(max_length=3, null=False)
     cotacao = models.FloatField(null=False)
     data = models.DateField(null=False)
+
+    def __str__(self) -> str:
+        return f"Moeda: {self.moeda} ** Cotacao: {self.cotacao} ** Data: {self.data}"
