cafe3332dc6d26b2a36a736cfd7b8d1c26dd56af
Ramon Brito <ramon.mecatec@gmail.com>
2023-06-23 13:09:56 -0300

    Redirecionar do index para o graficos/

--- cotacao_dolar/cotacao_dolar/urls.py
+++ cotacao_dolar/cotacao_dolar/urls.py
@@ -16,11 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('graficos/', include('graficos.urls')),
-    path('graficos/cotacoes/', include('graficos.urls')),
-    path(r'api/', include('api.urls')),
+
+    path('graficos/', include(('graficos.urls', 'graficos'), namespace='graficos')),
+    path('api/', include('api.urls')),
+    path('', RedirectView.as_view(
+        pattern_name='graficos:graficos-cotacoes', permanent=True))
 ]
