6008d97e88b6b65e40c64de949a874b17471b5c5
Ramon Brito <ramon.mecatec@gmail.com>
2023-06-25 18:32:51 -0300

    Adicionar nomes aos url patterns restantes do graficos.urls

--- cotacao_dolar/graficos/urls.py
+++ cotacao_dolar/graficos/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.graficos, name='graficos-cotacoes'),
-    path('cotacoes/', views.cotacoes),
-    path('cotacoes/database', views.cotacoes_banco_de_dados),
+    path('cotacoes/', views.cotacoes, name='cotacoes'),
+    path('cotacoes/database', views.cotacoes_banco_de_dados,
+         name='cotacoes-database'),
 ]
