d7235b4cf61c873bce0e4369b6a3b22ed1a6aa98
Ramon Brito <ramon.mecatec@gmail.com>
2023-06-25 18:51:20 -0300

    Add nome aos url patterns do api.urls

--- cotacao_dolar/api/urls.py
+++ cotacao_dolar/api/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('dias_uteis/obter', views.dias_uteis),
-    path('cotacao', views.ler_cotacao_banco_de_dados)
+    path('dias_uteis/obter', views.dias_uteis, name='obter_dias_uteis'),
+    path('cotacao', views.ler_cotacao_banco_de_dados, name='cotacao_database')
 ]
